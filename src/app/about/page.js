911bc7233a944b3a259a3fccab89257babe108f7
HarshitaBrandshow <harshita.trainee@brandshow.in>
2025-09-03 18:25:46 +0530

    new change

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -162,20 +162,6 @@ export default function About() {
                         </motion.div>
                     </div>
 
-                    {/* Scroll indicator */}
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 1, delay: 1.5 }}
-                        className="absolute bottom-10 left-1/2 transform -translate-x-1/2"
-                    >
-                        <div className="animate-bounce">
-                            <div className="text-white text-sm mb-2">Scroll down</div>
-                            <svg className="w-6 h-6 text-white mx-auto" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M19 14l-7 7m0 0l-7-7m7 7V3" />
-                            </svg>
-                        </div>
-                    </motion.div>
                 </div>
             </section>
 
